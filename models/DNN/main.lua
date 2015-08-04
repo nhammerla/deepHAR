85e47973c08b85769df3041acfd5684b79ee0ed8
Shane Work <shanework@Shanes-MacBook-Air.local>
2015-08-04 14:56:14 +0100

    Added epochs and GPU
    Added
     * Epochs: keeps training until either MaxEpoch is reached or else the
    performance (meanF1Score) has converged.
     * GPU
    
    Still doesn't use all command like parameters.

--- models/DNN/main.lua
+++ models/DNN/main.lua
@@ -62,19 +62,18 @@
 -- -- done!
 
 params={}
-params.numLayers = 2
-params.layerSize = 3
+params.numLayers = 1
+params.layerSize = 512
 params.dropout=0.5
-params.cpu = true
-params.seed=1
+params.cpu = false
+params.seed=123
 params.gpu= 1
-params.layerSize = 3
 params.batchSize = 64
-params.maxEpoch=10
+params.maxEpoch=25
+params.learningRate = 0.5
 require 'cunn'
 require 'nn'
 require 'cutorch'
-require 'optim'
 
 data = torch.load('opp1.dat' )
 inputLayerSize = data.training.inputs:size(2)*data.training.inputs:size(3)
@@ -84,8 +83,6 @@ torch.manualSeed(params.seed)
 cutorch.setDevice(params.gpu)
 cutorch.manualSeed(params.seed, params.gpu)
 torch.setnumthreads(16)
-epochPerformance = {} -- table to store progress
-testPerformance = {} -- table to store progress
 
 setmetatable(data.training, 
     {__index = function(t, i) 
@@ -99,9 +96,10 @@ end
 
 net = nn.Sequential()
 
-data.training.inputs = nn.Reshape(data.training.inputs:size(2)*data.training.inputs:size(3)):forward(data.training.inputs)
+--data.training.inputs = nn.Reshape(data.training.inputs:size(2)*data.training.inputs:size(3)):forward(data.training.inputs)
 
 --double-check inputLayerSize
+net:add(nn.Reshape(data.training.inputs:size(2)*data.training.inputs:size(3)))
 layerToAdd = nn.Linear(inputLayerSize, params.layerSize)
 net:add(layerToAdd)
 net:add(nn.Dropout(dropout))
@@ -112,22 +110,121 @@ outputSize = #data.classes
 net:add(nn.Linear(params.layerSize, outputSize))
 net:add(nn.LogSoftMax())
 
-criterion = nn.ClassNLLCriterion()
+criterion = nn.ClassNLLCriterion(torch.ones(18))
+
+if params.cpu==false then
+	--criterion = 
+	criterion:cuda()
+	data.training.inputs = data.training.inputs:cuda()
+	data.test.inputs = data.test.inputs:cuda()
+	data.validation.inputs = data.validation.inputs:cuda()
+	net:cuda()
+end
+
+parameters, gradParameters = net:getParameters()
+parameters:uniform(-0.08,0.08)
+
+lastValueOfList = function(list1)
+	return list1[#list1]
+end
+
+lowestOfLast_n_Values = function(list1,n) 
+	if not list1 then return nil end
+	lowest = list1[#list1-n+1]
+	for i=(#list1-n+1),#list1 do
+		if list1[i]<lowest then
+			lowest = list1[i]
+		end
+	end
+	return lowest
+end
+
+hasConverged = function(epoch_loss_values_list, n)
+	if #epoch_loss_values_list<11 then
+		return false
+	elseif lastValueOfList(epoch_loss_values_list)<lowestOfLast_n_Values(epoch_loss_values_list, n) then
+		return true
+	else
+		return false
+	end
+end
+
+epoch_loss_values_list = {}
 
 require 'stratBatchIter'
-labels = data.training.targets
--- AIM: use stratified batches on the GPU for one epoch, and then calcualte the epoch performance
-batchCounter = 1
-net:training()
-nbatch = torch.floor(data.training.targets:size(1) / params.batchSize)
-for batchIndex in stratBatchIter(labels, params.batchSize) do
-	inputBatch = data.training.inputs:index(1, batchIndex)
-	outputBatch = labels:index(1,batchIndex)
-	criterion:forward(net:forward(inputBatch), outputBatch)--3 seconds per epoch
-	net:zeroGradParameters()
-	net:backward(inputBatch, criterion:backward(net.output, outputBatch))
-	net:updateParameters(0.01)
-	xlua.progress(batchCounter, nbatch)
-	batchCounter = batchCounter +1
+require 'meanF1score'
+
+for epochNumber=1,params.maxEpoch do
+	converged = hasConverged(epoch_loss_values_list,10)
+	if converged then
+		break
+	else
+		--EPOCH BEGINS HERE
+		print('Training epoch '..epochNumber)
+		--TRAINING PHASE OF EPOCH
+		batchCounter = 1
+		net:training()
+		nbatch = torch.floor(data.training.targets:size(1) / params.batchSize)
+		for batchIndex in stratBatchIter(data.training.targets, params.batchSize) do
+			local batchInputs = data.training.inputs:index(1, batchIndex)
+			local batchTargets = data.training.targets:index(1,batchIndex)
+			if params.cpu == false then
+				batchTargets = batchTargets:cuda()
+			end
+			local batchPredictions = net:forward(batchInputs)
+			--print(batchPredictions)
+
+			-- WHAT DO THESE 4 LINES OF CODE DO?
+			--criterion:forward(batchPredictions, batchTargets)
+			--net:zeroGradParameters()
+			--net:backward(batchInputs, criterion:backward(net.output, batchTargets))
+			--net:updateParameters(0.01)
+
+			--print(batchTargets)
+
+			err = criterion:forward(batchPredictions, batchTargets)
+			gradCriterion = criterion:backward(batchPredictions, batchTargets)
+			net:zeroGradParameters()
+			net:backward(batchInputs, gradCriterion)
+			net:updateParameters(params.learningRate)
+
+			xlua.progress(batchCounter, nbatch)
+			batchCounter = batchCounter + 1
+		end
+		--TESTING PHASE OF EPOCH
+		--in this epoch, construct the confusion matrix
+		print('testing epoch...')
+		require 'optim'
+		cmatrix = optim.ConfusionMatrix(data.classes)
+		cmatrix:zero()
+
+		nbatch = torch.floor(data.validation.targets:size(1) / params.batchSize)
+		for batchIndex in stratBatchIter(data.validation.targets, params.batchSize) do
+			local testBatchInputs = data.validation.inputs:index(1, batchIndex)
+			local testBatchTargets = data.validation.targets:index(1,batchIndex):view(batchIndex:size(1))
+			local testBatchPredictions = net:forward(testBatchInputs)
+			--turn into softmax...
+			testBatchPredictions:exp() -- exponentiate
+			testBatchPredictions:cdiv(testBatchPredictions:sum(2):expandAs(testBatchPredictions)) -- divide by sum
+
+			--modify confusion matrix...
+			--cmatrix:batchAdd(testBatchPredictions, testBatchTargets)
+			--TRY GOING THROUGH IT INDIVIDUALLY INSTEAD!!
+			for i=1,testBatchPredictions:size(1) do
+				cmatrix:add(testBatchPredictions[i], testBatchTargets[i])
+			end
+		end
+
+		epochPerformance = meanF1score(cmatrix)
+		print('epoch performance ' .. epochPerformance)
+		table.insert(epoch_loss_values_list, epochPerformance)
+
+		--EPOCH ENDS HERE
+	end
 end
+print(cmatrix)
+print(epoch_loss_values_list)
+
+
+
 
