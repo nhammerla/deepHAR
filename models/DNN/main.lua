a6fe6869c1bf3c5e1da5c67810db547efe1119ce
Shane Work <shanework@Shanes-MacBook-Air.local>
2015-08-05 22:00:45 +0100

    Added maxInNorm, patience

--- models/DNN/main.lua
+++ models/DNN/main.lua
@@ -22,7 +22,7 @@ cmd:text()
 cmd:text('Options')
 cmd:option('-seed',                   123,    'initial random seed')
 cmd:option('-logdir',                 'exp',  'path to store model progress, results, and log file')
-cmd:option('-data',                   'opp1.dat',     'data-set to run on (DP datasource)')
+cmd:option('-datafile',                   'opp1.dat',     'data-set to run on (DP datasource)')
 cmd:option('-gpu',                    1,      'GPU to run on (default: 1)') -- NOTE:1
 cmd:option('-cpu',                    false,  'Run on CPU')
 cmd:option('-numLayers',              1,      'Number of hidden layers')
@@ -30,10 +30,10 @@ cmd:option('-layerSize',              512,    'Number of units in hidden layers'
 cmd:option('-learningRate',           0.5,    'Learning rate')
 cmd:option('-batchSize',              64,     'Batch-size')
 cmd:option('-dropout',                0.5,    'Dropout')
--- cmd:option('-momentum',               0.9,    'Momentum')
+--cmd:option('-momentum',               0.9,    'Momentum')
 -- cmd:option('-learningRateDecay',      5e-4,   'Learning rate decay')
--- cmd:option('-maxInNorm',              3,      'Max-in-norm for regularisation')
--- cmd:option('-patience',               10,     'Patience in early stopping')
+cmd:option('-maxInNorm',              3,      'Max-in-norm for regularisation')
+cmd:option('-patience',               10,     'Patience in early stopping')
 cmd:option('-minEpoch',               30,     'Minimum number of epochs before check for convergence')
 cmd:option('-maxEpoch',               150,    'Stop after this number of epochs even if not converged')
 -- cmd:option('-ignore',                 false,  'Is there a class we should ignore?')
@@ -52,7 +52,7 @@ paths.mkdir(params.rundir)
 cmd:log(params.rundir .. '/log', params)
 
 -- -- Read in data-set and (maybe) store on GPU
-data = torch.load(data)
+data = torch.load(params.datafile)
 -- -- Define model
 
 -- -- helper functions
@@ -115,6 +115,22 @@ end
 parameters, gradParameters = net:getParameters()
 parameters:uniform(-0.08,0.08)
 
+renormW = function(mod)
+    -- rescale to incoming 2-norm of maxInNorm for each hidden unit
+    local par = mod:parameters()
+
+    -- skip layers without parameters (e.g. ReLU())
+    if not par then
+        return
+    end
+    for i,p in pairs(par) do
+        if p:dim() > 1 then
+            -- layer weights
+            p:renorm(2,p:dim(),params.maxInNorm)
+        end
+    end
+end
+
 lastValueOfList = function(list1)
 	return list1[#list1]
 end
@@ -145,7 +161,7 @@ epoch_loss_values_list = {}
 bestPerformance = 0
 
 for epochNumber=1,params.maxEpoch do
-	converged = hasConverged(epoch_loss_values_list,10)
+	converged = hasConverged(epoch_loss_values_list,params.patience)
 	if converged and not epochNumber<params.minEpoch then
 		break
 	else
@@ -178,12 +194,16 @@ for epochNumber=1,params.maxEpoch do
 			net:backward(batchInputs, gradCriterion)
 			net:updateParameters(params.learningRate)
 
+			-- renormalise weights
+     		for i,mod in ipairs(net.modules) do
+       			renormW(mod)
+     		end
 			--xlua.progress(batchCounter, nbatch)
 			--batchCounter = batchCounter + 1
 		end
 		--TESTING PHASE OF EPOCH
 		--in this epoch, construct the confusion matrix
-		
+
 		--print('Testing epoch...')
 		
 		cmatrix = optim.ConfusionMatrix(data.classes)
@@ -222,6 +242,7 @@ for epochNumber=1,params.maxEpoch do
 	xlua.progress(epochCounter, params.maxEpoch)
 	epochCounter = epochCounter+1
 end
+print()
 print(cmatrix)
 print(epoch_loss_values_list)
 
