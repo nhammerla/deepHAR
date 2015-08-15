0db9b56eba334226ad9bf8747bae06c5275cd8c9
shaneh2 <shaneh2@users.noreply.github.com>
2015-08-15 20:33:07 +0100

    Create training, validation and test sets (Matlab)
    Currently operating on the modified Opportunity dataset available from 
    http://www.opportunity-project.eu/challengedatasetdownload 
    Zip file: http://www.opportunity-project.eu/system/files/Challenge/OpportunityChallengeLabeled.zip
    , because it just contains the 113-dimension body work sensor data. The labels are included in this dataset.
    
    I still need to create a rolllingWindows(data, windowSize, stepSize) function, which will additionally properly align the accompanying labels.
    
    Assuming this splitting for creation of train, validation and test sets:
     * Training set should be made up of ADL1 to ADL3 for people 1, 2 and 3 (all people).
     * Validation set should be made up of ADL4 and ADL5 for person 1.
     * Test set should be made up of ADL4 and ADL5 for person 3.
    The drill runs aren't used.

---
+++ data/shaneMakeSets.lua
@@ -0,0 +1,49 @@
+% Training data
+load('S1-ADL1.dat')
+load('S1-ADL2.dat')
+load('S1-ADL3.dat')
+
+load('S2-ADL1.dat')
+load('S2-ADL2.dat')
+load('S2-ADL3.dat')
+
+load('S3-ADL1.dat')
+load('S3-ADL2.dat')
+load('S3-ADL3.dat')
+
+trainingX = [S1_ADL1;S1_ADL2;S1_ADL3;  S2_ADL1;S2_ADL2;S2_ADL3; S3_ADL1;S3_ADL2;S3_ADL3];
+trainingData = trainingX(:,2:114);
+trainingLabels = trainingX(:,116);
+
+%Validation data
+load('S1-ADL4.dat')
+load('S1-ADL5.dat')
+
+valX = [S1_ADL4;S1_ADL5];
+valData = valX(:,2:114);
+valLabels = valX(:,116);
+
+% Testing data
+load('S2-ADL4.dat')
+load('S2-ADL5.dat')
+
+load('S3-ADL4.dat')
+load('S3-ADL5.dat')
+
+testingX = [S2_ADL4;S2_ADL5; S3_ADL4;S3_ADL5];
+testingData = testingX(:,2:114);
+testingLabels = testingX(:,116);
+
+%Make rolling windows from data matrices:
+testingData = rollingWindows(testingData, 15, 30)
+valData = rollingWindows(valData, 15, 30)
+trainingData = rollingWindows(trainingData, 15, 30)
+
+save('trainingData.mat', 'trainingData')
+save('trainingLabels.mat', 'trainingLabels')
+
+save('testingData.mat', 'testingData')
+save('testingLabels.mat', 'testingLabels')
+
+save('valData.mat', 'valData')
+save('valLabels.mat', 'valLabels')
