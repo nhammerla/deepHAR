5eec66c0d2d2a5411caa13163aeaa26d6a1d3904
Shane Work <shanework@Shanes-MacBook-Air.local>
2015-08-18 15:22:34 +0100

    Added rollingWindows
    rollingWindows function takes a data matrix and creates rolling windows
    with specified window and step sizes. rollingWindows also takes in an
    accompanying labels vector, which will align with the end of each
    rolling window.
    
    repelem.m is necessary to repeat elements of the labels vector on
    matlab<2015

---
+++ data/rollingWindows.m
@@ -0,0 +1,28 @@
+%A function to make rolling windows from a matrix
+function [output,labelsOutput] = rollingWindows(matrix, labelsVector, stepSize, windowSize);
+    assert(size(matrix,1)==length(labelsVector), 'The labels vector should be the same size as the first dimension of input matrix, but is not')
+    transposedMatrix = transpose(matrix);
+    unrolledMatrix = transpose( transposedMatrix(:) );
+
+    labelsVector = repelem(labelsVector, size(matrix,2));
+
+    %Creating first row of the output matrix:
+    temp=unrolledMatrix(1:windowSize);
+    labelsTemp = labelsVector(windowSize);
+
+    %Setting up next rows:
+    startpoint=stepSize+1;
+    endpoint=startpoint+windowSize-1;
+    while endpoint<=size(unrolledMatrix,2)
+        rowToAdd = unrolledMatrix(startpoint:endpoint);
+        temp = [temp;rowToAdd];
+        labelsTemp = [labelsTemp labelsVector(endpoint)];
+        %after appending this new row, set new startpoints and endpoints:
+        startpoint = startpoint + stepSize;
+        endpoint = startpoint+windowSize-1;
+        if mod(endpoint, 1000)==0
+                sprintf('%d / %d', endpoint, size(unrolledMatrix, 2))
+    end
+    output = temp;
+    labelsOutput = labelsTemp;
+end
--- data/shaneMakeSets.lua
+++ data/shaneMakeSets.lua
@@ -1,4 +1,4 @@
-% Training data
+% Training datasets
 load('S1-ADL1.dat')
 load('S1-ADL2.dat')
 load('S1-ADL3.dat')
@@ -15,7 +15,7 @@ trainingX = [S1_ADL1;S1_ADL2;S1_ADL3;  S2_ADL1;S2_ADL2;S2_ADL3; S3_ADL1;S3_ADL2;
 trainingData = trainingX(:,2:114);
 trainingLabels = trainingX(:,116);
 
-%Validation data
+%Validation sets
 load('S1-ADL4.dat')
 load('S1-ADL5.dat')
 
@@ -23,7 +23,7 @@ valX = [S1_ADL4;S1_ADL5];
 valData = valX(:,2:114);
 valLabels = valX(:,116);
 
-% Testing data
+% Testing datasets
 load('S2-ADL4.dat')
 load('S2-ADL5.dat')
 
@@ -34,10 +34,15 @@ testingX = [S2_ADL4;S2_ADL5; S3_ADL4;S3_ADL5];
 testingData = testingX(:,2:114);
 testingLabels = testingX(:,116);
 
+
+
 %Make rolling windows from data matrices:
-testingData = rollingWindows(testingData, 15, 30)
-valData = rollingWindows(valData, 15, 30)
-trainingData = rollingWindows(trainingData, 15, 30)
+disp('making rolling windows for testing data')
+[testingData,testingLabels] = rollingWindows(testingData,testingLabels, 15, 30)
+disp('making rolling windows for validation data')
+[valData,valLabels] = rollingWindows(valData,valLabels, 15, 30)
+disp('making rolling windows for training data')
+[trainingData,trainingLabels] = rollingWindows(trainingData, trainingLabels, 15, 30)
 
 save('trainingData.mat', 'trainingData')
 save('trainingLabels.mat', 'trainingLabels')
