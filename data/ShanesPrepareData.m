1554f1ab0d726a6f85950bd45f44d527d80d0c9a
shaneh2 <s.halloran1@ncl.ac.uk>
2015-10-27 15:39:19 +0000

    Using data from all subjects

--- data/ShanesPrepareData.m
+++ data/ShanesPrepareData.m
@@ -5,6 +5,7 @@ clear;
 normalize = true;
 makeMeanFeatures = true;
 convertToTorch = true;
+allSubjects = true;
 %Be sure to cd into this directory first!
 %base='../../dataset/';
 base=('~/OpportunityUCIDataset/dataset/')
@@ -12,19 +13,61 @@ addpath(genpath('~/OpportunityUCIDataset/scripts/'))
 
 selectedCol = [2:46 51:59 64:72 77:85 90:98 103:134 250];
 
-% Subject 1
-% training
-sadl1 = load([base 'S1-ADL1.dat']);
-sadl2 = load([base 'S1-ADL2.dat']);
-sadl3 = load([base 'S1-ADL3.dat']);
-sdrill = load([base 'S1-Drill.dat']);
-% test
-sadl4 = load([base 'S1-ADL4.dat']);
-sadl5 = load([base 'S1-ADL5.dat']);
+if allSubjects
+	% training
+	%Subject 1
+	s1adl1 = load([base 'S1-ADL1.dat']);
+	s1adl2 = load([base 'S1-ADL2.dat']);
+	s1adl3 = load([base 'S1-ADL3.dat']);
+	s1adl4 = load([base 'S1-ADL4.dat']);
+	s1adl5 = load([base 'S1-ADL5.dat']);
+	s1drill = load([base 'S1-Drill.dat']);
 
-[training1,test1]=tarrange(4,sadl1,sadl2,sadl3,sdrill,sadl4,sadl5);
+	%Subject 2
+	s2adl1 = load([base 'S2-ADL1.dat']);
+	s2adl2 = load([base 'S2-ADL2.dat']);
+	s2adl3 = load([base 'S2-ADL3.dat']);
+	s2drill = load([base 'S2-Drill.dat']);
+	%Subject 3
+	s3adl1 = load([base 'S3-ADL1.dat']);
+	s3adl2 = load([base 'S3-ADL2.dat']);
+	s3adl3 = load([base 'S3-ADL3.dat']);
+	s3drill = load([base 'S3-ADL3.dat']);
+	%Subject 4
+	s4adl1 = load([base 'S4-ADL1.dat']);
+	s4adl2 = load([base 'S4-ADL2.dat']);
+	s4adl3 = load([base 'S4-ADL3.dat']);
+	s4drill = load([base 'S4-ADL4.dat']);
+
+	% test
+	%Subject 2
+	s2adl4 = load([base 'S2-ADL4.dat']);
+	s2adl5 = load([base 'S2-ADL5.dat']);
+	%Subject 3
+	s3adl4 = load([base 'S3-ADL4.dat']);
+	s3adl5 = load([base 'S3-ADL5.dat']);
+	%Subject 4
+	s4adl4 = load([base 'S4-ADL4.dat']);
+	s4adl5 = load([base 'S4-ADL5.dat']);
+	[training1,test1]=tarrange(18, s1adl1, s1adl2, s1adl3, s1adl4, s1adl5, s1drill, s2adl1, s2adl2, s2adl3, s2drill, s3adl1, s3adl2, s3adl3, s3drill, s4adl1, s4adl2, s4adl3, s4drill, s2adl4, s2adl5, s3adl4, s3adl5, s4adl4, s4adl5);
+
+else
+	% Subject 1
+	% training
+	sadl1 = load([base 'S1-ADL1.dat']);
+	sadl2 = load([base 'S1-ADL2.dat']);
+	sadl3 = load([base 'S1-ADL3.dat']);
+	sdrill = load([base 'S1-Drill.dat']);
+	% test
+	sadl4 = load([base 'S1-ADL4.dat']);
+	sadl5 = load([base 'S1-ADL5.dat']); 
+	[training1,test1]=tarrange(4,sadl1,sadl2,sadl3,sdrill,sadl4,sadl5);
+end
+
 training1 = training1(:,selectedCol);
 test1 = test1(:,selectedCol);
+
+clear s1* s2* s3* s4* 
 
 %training data without labels:
 trainingData = training1(:,1:(end-1));
@@ -42,7 +85,7 @@ testingLabels = test1(:,end);
 uniques = unique(sort(testingLabels));
 testingLabels = changem(testingLabels, 1:length(uniques), uniques);
 
-clear base sadl1 sadl2 sadl3 sadl4 sadl5 sdrill selectedCol test1 training1 uniques
+clear training1 test1
 
 % ---CODE TO REMOVE THE NULL CLASS---
 %isNULL = trainingLabels==1;
@@ -53,17 +96,19 @@ clear base sadl1 sadl2 sadl3 sadl4 sadl5 sdrill selectedCol test1 training1 uniq
 trainingData = backfillnans(trainingData); testingData = backfillnans(testingData);
 
 if normalize 
-	[trainingData, colMeans, colStds] = normalizeMatrix(trainingData);
+%	[trainingData, colMeans, colStds] = normalizeMatrix(trainingData);
 
 	%Normalize the test data using the same column means and standard deviations as in the training set:
-	testingData = (testingData - repmat(colMeans, size(testingData,1),1)) ./ repmat(colStds, size(testingData,1), 1);
-	testingData(isnan(testingData)) = 0;
+%	testingData = (testingData - repmat(colMeans, size(testingData,1),1)) ./ repmat(colStds, size(testingData,1), 1);
+%	testingData(isnan(testingData)) = 0;
+	trainingData = trainingData./1000;
+	testingData = testingData./1000;
 end
 
 stepSize = 15;
 windowSize = 30;
 
-classes = unique(trainingLabels)
+classes = unique(trainingLabels);
 [trainingData, trainingLabels] = rollingWindows(trainingData, trainingLabels, stepSize, windowSize);
 [testingData, testingLabels] = rollingWindows(testingData, testingLabels, stepSize, windowSize);
 %trainingData = trainingData';testingData = testingData';
