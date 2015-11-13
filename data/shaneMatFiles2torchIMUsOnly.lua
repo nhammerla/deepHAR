184b6beb4e3e0a0c10aab0f8474e079625d93607
shaneh2 <s.halloran1@ncl.ac.uk>
2015-11-13 16:21:32 +0000

    Make seperate validation and test sets

--- data/shaneMatFiles2torchIMUsOnly.lua
+++ data/shaneMatFiles2torchIMUsOnly.lua
@@ -14,11 +14,10 @@ data.classes = torch.totable(torch.range(1,18))
 data.training.inputs = loading.trainingData
 data.training.targets = torch.squeeze(loading.trainingLabels)
 
--- Make test and validation sets the same for now...
+data.validation.inputs = loading.valData 
+data.validation.targets = torch.squeeze(loading.valLabels) 
+
 data.test.inputs = loading.testingData
 data.test.targets = torch.squeeze(loading.testingLabels)
-
-data.validation.inputs = data.test.inputs
-data.validation.targets = data.test.targets
 
 torch.save('opp2IMUsOnly.dat',data)
