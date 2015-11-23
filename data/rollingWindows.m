2376bd9214525cbfb460ca954108215edc9a7811
shaneh2 <s.halloran1@ncl.ac.uk>
2015-11-23 18:50:04 +0000

    Simplified rolling windows code

--- data/rollingWindows.m
+++ data/rollingWindows.m
@@ -1,8 +1,6 @@
 function [slidingWindows, labelsForWindows] = rollingWindows(matrix, labelsVector, stepSize, windowLength);
 	% matrix function to make 3D rolling windows matrix from a 2D matrix
 	
-%	matrix = matrix(1:cutoff, :);
-%	labelsVector= labelsVector(1:cutoff);
 	%disp('Input Matrix Size');size(matrix)
 	%disp('labelsVector');size(labelsVector)
 	
@@ -23,28 +21,12 @@ function [slidingWindows, labelsForWindows] = rollingWindows(matrix, labelsVecto
 	maxPossibleNumOfWindows = ceil(1+(numberOfRows/stepSize));
 	%pre-allocate 3D array:
 	B=zeros(windowLength, size(matrix,2),maxPossibleNumOfWindows);
-	%NOTE: THIS IS DIFFERENT FORMmatrixT TO NILS' DATA.
-	%30x113x11321
-	%parfor rowNumber=1:(numberOfWindows)
-	%    B(:,:,rowNumber) = matrix(rowNumber:(rowNumber+windowLength-1),:);
-	%    %B(rowNumber,:,:) = matrix(rowNumber:(rowNumber+windowLength),:)
-	%end
-	
-	%workings begin...
-	%B(:,:,1) = matrix(1:(30),:);1+ (i-1)*windowSize
-	%B(:,:,2) = matrix(15:(45),:);1+ (i-1)*windowSize - (i-1)*overlap
-	%B(:,:,3) = matrix(31:(60),:);1+ (i-1)*windowSize - (i-1)*overlap
-	%B(:,:,4) = matrix(46:(75),:);
-	%workings end...
-
 	numOfWindows = 1;
-	
 
 	%pre-allocate the labels vector:
 	labels = squeeze(zeros(maxPossibleNumOfWindows,1));
 	
 	for i=1:maxPossibleNumOfWindows
-		%windowBeginsAt = 1 + ( (i-1)*windowLength) - ( (i-1)*stepSize);
 		windowBeginsAt = 1 + ((i-1)*stepSize);
 		windowEndsAt = windowBeginsAt + windowLength - 1;
 		if windowEndsAt<=length(labelsVector)
