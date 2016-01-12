11871feb382570b21d8e7b064a26848c04691dc1
shaneh2 <s.halloran1@ncl.ac.uk>
2016-01-12 16:01:20 +0000

    proper labels automatically in fanova for plots of marginal effects

--- eval/analysisScript/makeReportDirectory.py
+++ eval/analysisScript/makeReportDirectory.py
@@ -1,6 +1,21 @@
 #Usage: python makeReportDirectory.py CSVFILENAME RESULTSFOLDERNAME MINF1SCORE QUANTILETOCOMPARE
 #For now, the CSV file must have a header row, and be in the same directory as this file.
 hasHeader = True
+import csv
+
+def headerList(csvFileName):
+    #My own way of extracting headers:
+    file1 = open(csvFileName, 'r') 
+    reader1 = csv.reader(file1)
+    headers=reader1.next()
+    if headers[0]=='id':
+            headers=headers[2:]
+    if 'mean f1' in headers:
+            headers.remove('mean f1')
+    headers = [('X'+str(i), headers[i]) for i in range(0,len(headers))]
+    print(headers)
+    file1.close()
+    return headers
 
 import sys
 
@@ -15,6 +30,7 @@ if hasHeader:
     os.system("Rscript process.R "+csvFileName+" "+minmeanF1score)
 
 os.system('mkdir '+resultsFolderName+'  > /dev/null 2>&1')
+headers = headerList(csvFileName)
 csvFileName = 'noHeader'+csvFileName
 print(csvFileName)
 os.system('python fanovaPrintOutput.py '+csvFileName+' > ./'+resultsFolderName+'/variableImportances.txt'+' '+quantileToCompare)
@@ -24,12 +40,40 @@ import matplotlib
 matplotlib.use('Agg')
 
 from pyfanova.fanova_from_csv import FanovaFromCSV
+import inspect
 f = FanovaFromCSV(csvFileName)#, improvement_over="QUANTILE", quantile_to_compare=0.25)
 
 from pyfanova.visualizer import Visualizer
 
 vis = Visualizer(f)
-vis.create_all_plots(resultsFolderName)
+#vis.create_all_plots(resultsFolderName)
+
+
+params = headers
+import matplotlib
+matplotlib.use('Agg')
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+from pyfanova.fanova_from_csv import FanovaFromCSV
+
+f = FanovaFromCSV("noHeaderall_dnn.csv")
+from pyfanova.visualizer import Visualizer
+vis = Visualizer(f)
+
+#For Continuous and Integer Parameters:
+for (paramX, param_desc) in params:
+        print('creating %s'%param_desc)
+        plt2 = vis.plot_marginal(paramX)
+        plt2.xlabel(param_desc)
+        plt2.savefig('./'+resultsFolderName+'/'+param_desc+'.jpg')
+
+
+
+
 vis.create_most_important_pairwise_marginal_plots(resultsFolderName, 5)
 
 os.system('killall java')
