2a8b6704f744bd1983a75615f438e6652003a994
shaneh2 <s.halloran1@ncl.ac.uk>
2016-02-01 13:44:43 +0000

    Remove duplicate rows in outputs from experiment runs

--- eval/analysisScript/process.R
+++ eval/analysisScript/process.R
@@ -6,7 +6,9 @@ fileToRead<-(args[1])
 minmeanF1score<-as.numeric(args[2])
 x = read.csv(fileToRead, sep=',', header=TRUE)
 x = x[,(1+numColsAtBeginToIgnore):(ncol(x)-numColsAtEndToIgnore)]
+x=unique(x)
 x = subset(x, x[,ncol(x)]>minmeanF1score) 
+#Ignore columns with same values:
 print('First 6 rows of results being processed:')
 head(x)
 write.table(x, paste('noHeader',fileToRead,sep=''),row.names=FALSE, col.names=FALSE,sep=',')
